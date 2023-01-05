8c0ed5b05c46aa1ec9f729dc4efbc961c069c1d9
Alexander Shabanov <99245672+netmarketolog@users.noreply.github.com>
2023-01-05 09:14:01 +0200

    Update PostsScreen.js

--- Screens/mainScreen/PostsScreen.js
+++ Screens/mainScreen/PostsScreen.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+
+import { MaterialIcons } from "@expo/vector-icons";
 
 const PostsScreen = () => {
   return (
     <View style={styles.container}>
       <Text>PostsScreen</Text>
+      <TouchableOpacity style={styles.logout}>
+        <MaterialIcons name="logout" size={24} color="black" />
+      </TouchableOpacity>
     </View>
   );
 };
@@ -13,6 +18,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  logout: {
+    position: "absolute",
+    right: 10,
+    top: 54,
+    opacity: 0.2,
   },
 });
 export default PostsScreen;
