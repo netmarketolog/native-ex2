21e98a4bcabd700e557e1f3a9d688d5c80c25adc
Alexander Shabanov <99245672+netmarketolog@users.noreply.github.com>
2023-05-13 04:17:04 +0300

    add btn open cam vs close

--- screens/mainScreen/PostScreen/CreatePostScreen.jsx
+++ screens/mainScreen/PostScreen/CreatePostScreen.jsx
@@ -5,8 +5,8 @@ import { Camera  } from "expo-camera";
 
 const CreatePostScreen = () => {
   const [permission, requestPermission] = Camera.useCameraPermissions();
-  const [imgRef, setImgRef] = useState(null);
-  const [photo, setPhoto] = useState(null);
+  const [imgRef, setImgRef] = useState(null); // Camera {}
+  const [photo, setPhoto] = useState(null); // Хранит снимок 
 
   if (!permission) {
     // Camera permissions are still loading
@@ -17,25 +17,28 @@ const CreatePostScreen = () => {
 
 
   const takePhoto = async () => {
-    const photo = await imgRef.takePictureAsync();
+    const photo = await imgRef.takePictureAsync(); // Делает снимок и сохраняет его в каталог кеша приложения.
     setPhoto(photo.uri);
   };
 
+
   return (
     <View style={styles.container}>
+      
         {!permission.granted ? (
-          <View style={{ flex: 1, marginTop: 120,}}>
-          <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
-          <TouchableOpacity
-            activeOpacity={0.7}
-            style={styles.btn}
-            onPress={requestPermission}
-          >
-            <Text style={styles.btnText}>Grant permission</Text>
-          </TouchableOpacity>
-        </View>
+          <View style={styles.request}>
+            <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+            <TouchableOpacity
+              activeOpacity={0.7}
+              style={styles.btn}
+              onPress={requestPermission}
+            >
+              <Text style={styles.btnText}>Grant permission</Text>
+            </TouchableOpacity>
+          </View>
         ):
-        (<Camera style={styles.camera} ref={setImgRef} >
+        (<View style={styles.containerCamera} >
+          <Camera style={styles.camera} ref={setImgRef} >
           {photo && (
           <View style={styles.takePhotoContainer}>
           <Image
@@ -44,11 +47,28 @@ const CreatePostScreen = () => {
           />
         </View>
         )}
-          <TouchableOpacity style={styles.cameraBtn} onPress={takePhoto}>
+          {!photo && (<TouchableOpacity style={styles.cameraBtn} onPress={takePhoto}>
             <Image source={require("../../../img/camera.png")} />
-          </TouchableOpacity>
-        </Camera>)
+          </TouchableOpacity>)}
+        </Camera>
+          <View style={{ flex: 1, marginTop: 8,}}>
+            {!photo ? (<TouchableOpacity
+              activeOpacity={0.7}
+              onPress={requestPermission}
+            >
+              <Text style={styles.btnTextCamera}>Загрузите фото</Text>
+            </TouchableOpacity>
+            ) : ( 
+            <TouchableOpacity
+              activeOpacity={0.7}
+              onPress={() => setPhoto(null)}
+            >
+              <Text style={styles.btnTextCamera}>Новое фото</Text>
+            </TouchableOpacity>)}
+          </View>
+        </View> ) 
         }
+      
 
     </View>
   );
@@ -57,6 +77,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
+  },
+  containerCamera:{
+    flex: 1,
+    
+    width: 340,
+    height: 270,
   },
   request:{
     flex: 1,
@@ -100,10 +126,18 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     },
-    btnText: {
+  btnText: {
     color: "#FFFFFF",
     fontSize: 16,
     lineHeight: 19,
+    },
+  btnTextCamera:{
+    width: 156,
+    height: 19,
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#BDBDBD",
     },
 });
 export default CreatePostScreen;
